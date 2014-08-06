9821f26dc3c0532a9b7038792540fa698012ec49
Francesco Colella <mnemonyc@hotmail.com>
2014-08-06 12:38:23 +0200

    Revert "Revert "camera: vector implementation needed by vendor camera""
    This reverts commit 5ad3f157069e277e04c544b627d24166d4993a5f.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,11 +54,6 @@ BOARD_SEPOLICY_UNION := \
 # assert
 TARGET_OTA_ASSERT_DEVICE := m0,i9300,GT-I9300
 
-TARGET_PROVIDES_CAMERA_HAL := true
-
-# inherit from the proprietary version
--include vendor/samsung/i9300/BoardConfigVendor.mk
-
 # added navbar
 ADDITIONAL_BUILD_PROPERTIES += \
     qemu.hw.mainkeys=0
@@ -67,4 +62,8 @@ ADDITIONAL_BUILD_PROPERTIES += \
 DEVICE_RESOLUTION := 720x1280
 
 # Camera wrapper
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 TARGET_PROVIDES_CAMERA_HAL := true
+
+# inherit from the proprietary version
+-include vendor/samsung/i9300/BoardConfigVendor.mk
